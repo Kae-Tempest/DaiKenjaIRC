b50e6c6e63fd56eedc498ba8946f61f0fc527864
Kae-Tempest <Katsuhiko.tempest@gmail.com>
2021-01-17 13:12:19 +0100

    fix

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const config = require('./config')
 const opts = {
     identity: {
         username: "Dai_Kenja",
-        password: "oauth:pz0756nq1q0c156duwdwf1j4pjsue2"
+        password: config.password
     },
     channels: ["katsuhiiko"]
 };
