81dd2f187585b69f8ea2f124317f8d28d0fda659
Kae-Tempest <Katsuhiko.tempest@gmail.com>
2021-01-20 18:12:59 +0100

    add some command

--- main.js
+++ main.js
@@ -1,33 +1,25 @@
 const tmi = require('tmi.js');
 const config = require('./config')
 const chalk = require("chalk");
-const colors = new chalk.Instance({ level: 3 });
-
+const colors = new chalk.Instance({level: 3});
 const opts = {
-    connection: {
-        reconnect: true,
-        secure: true,
-    },
-    identity: {
-        username: "Dai_Kenja",
-        password: config.password
-    },
+    connection: {reconnect: true, secure: true,},
+    identity: {username: "Dai_Kenja", password: config.password},
     channels: ["katsuhiiko"]
 };
-
 const client = new tmi.client(opts)
 client.connect();
-
-client.on("chat",(channel, userstate, message, self)=>{
+client.on("chat", (channel, userstate, message, self) => {
     if (self) return;
     const user = userstate.username;
     const text = message.trim();
     const color = userstate.color
     const nick = colors.hex(color)(`${user}`);
     const arrow = '=>'
-    console.log(`${nick} ${arrow} ${text}`);
+    console.log(`${channel} ${arrow} ${nick} ${arrow} ${text}`);
 })
-client.on('message', (target, context, msg, self)=>{
+
+client.on('message', (target, context, msg, self) => {
     if (self) return;
     const commandName = msg.trim();
     if (commandName === config.prefix + 'dice') {
@@ -35,9 +27,14 @@ client.on('message', (target, context, msg, self)=>{
         let num = Math.floor(Math.random() * sides) + 1;
         client.say(target, ` You rolled => ${num}`)
     }
-
     if (commandName === config.prefix + 'hi') client.say(target, 'Hello World!');
+    const discordLink = 'https://discord.gg/V9t5k5z'
+    if (commandName === config.prefix + 'discord') client.say(target, `Lien pour rejoindre mon discord => ${discordLink}`)
+    if (commandName === config.prefix + 'skarab') client.say(target, "Va chez suivre Skarab ou j'te tape Kappa (https://www.twitch.tv/skarab42)")
 });
-client.on('connected', (addr, port) =>{
-    console.log(`* Connected to ${addr}:${port}`);
+client.on('connected', () => {
+    let now = new Date().toLocaleString('fr-FR')
+    console.clear()
+    console.log('\033[2J')
+    console.log(` ${opts.identity.username} connected le ${now}`)
 });
