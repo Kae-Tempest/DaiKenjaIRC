90ba748009e3b231655e306fa2c979b0dd37716d
Kae-Tempest <Katsuhiko.tempest@gmail.com>
2021-01-20 15:08:16 +0100

    fix

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     prefix: '!',
-    password: "oauth:pz0756nq1q0c156duwdwf1j4pjsue2"
+    password: "oauth:pz0756nq1q0c156duwdwf1j4pjsue2",
+    token: "NzgzNzM3ODYyODcxODQyODI2.X8fGww.2m4IWMuFUV9xvv5G1Ly3m8sahSA"
 }
--- main.js
+++ main.js
@@ -10,29 +10,25 @@ const opts = {
 };
 
 const client = new tmi.client(opts)
-
-// Register our event handlers (defined below)
 client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
 
-// Connect to Twitch:
 client.connect();
-// Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-    if (self) { return; } // Ignore messages from the bot
-    const PREFIX = "!"
-    // Remove whitespace from chat message
-    const commandName = msg.trim();
 
-    // If the command is known, let's execute it
-    if (commandName === config.prefix + 'dice') {
-        const sides = 6;
-        let num = Math.floor(Math.random() * sides) + 1;
-        client.say(target, `You rolled a ${num}`);
+function onMessageHandler(target, context, msg, self) {
+    if (self) {
+        return;
     }
-    if (commandName === config.prefix + 'hi') client.say(target, 'Hello World!');
+    const commandName = msg.trim();
+    if (commandName === config.prefix + 'dice') {
+            const sides = 6;
+            let num = Math.floor(Math.random() * sides) + 1;
+            client.say(target, ` You rolled => ${num}`)
+    }
+
+    if (commandName === config.prefix + 'hi')client.say(target, 'Hello World!');
 }
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
+
+function onConnectedHandler(addr, port) {
     console.log(`* Connected to ${addr}:${port}`);
 }
