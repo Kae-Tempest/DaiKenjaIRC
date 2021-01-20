e8080274cfea8b622477158fde740503d646dd7f
Kae-Tempest <Katsuhiko.tempest@gmail.com>
2021-01-20 15:49:41 +0100

    add chat in IDE console

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const tmi = require('tmi.js');
 const config = require('./config')
+const chalk = require("chalk");
+const colors = new chalk.Instance({ level: 3 });
 
 const opts = {
     identity: {
@@ -16,17 +18,22 @@ client.on('connected', onConnectedHandler);
 client.connect();
 
 function onMessageHandler(target, context, msg, self) {
+    const user = target;
+    const text = msg.trim();
+    const nick = (`${user}`);
+    console.log(`${nick} => ${text}`);
+
     if (self) {
         return;
     }
     const commandName = msg.trim();
     if (commandName === config.prefix + 'dice') {
-            const sides = 6;
-            let num = Math.floor(Math.random() * sides) + 1;
-            client.say(target, ` You rolled => ${num}`)
+        const sides = 6;
+        let num = Math.floor(Math.random() * sides) + 1;
+        client.say(target, ` You rolled => ${num}`)
     }
 
-    if (commandName === config.prefix + 'hi')client.say(target, 'Hello World!');
+    if (commandName === config.prefix + 'hi') client.say(target, 'Hello World!');
 }
 
 function onConnectedHandler(addr, port) {
