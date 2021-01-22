15d7f13d4eb8d8b0a6143571c5fd5aaa2bc70c45
Kae-Tempest <Katsuhiko.tempest@gmail.com>
2021-01-22 21:30:56 +0100

    remove cmd

--- main.js
+++ main.js
@@ -26,27 +26,14 @@ client.on("chat", (channel, userstate, message, self) => {
 
 })
 
-client.on('message', (target, context, msg, self) => {
+client.on('message', (target, context, msg, self, userstate) => {
     if (self) return;
     const commandName = msg.trim();
     const discordLink = 'https://discord.gg/V9t5k5z'
-    if (commandName === config.prefix + 'dice') {
-        const sides = 6;
-        let num = Math.floor(Math.random() * sides) + 1;
-        client.say(target, ` You rolled => ${num}`)
-    }
-
-    let commandNameList = ['dice','hi','discord','skarab','purple','git','hi skarab','battle','help'];
-
-    if (commandName === config.prefix + 'hi') client.say(target, 'Hello World!');
     if (commandName === config.prefix + 'discord') client.say(target, `Lien pour rejoindre mon discord => ${discordLink}`)
-    if (commandName === config.prefix + 'skarab') client.say(target, "Va suivre Skarab ou j'te tape Kappa \n (https://www.twitch.tv/skarab42)")
-    if (commandName === config.prefix + 'purple') client.say(target, "Va suivre Purple ou j'te tape Kappa \n (https://www.twitch.tv/purpleorwel)")
+    if (commandName === config.prefix + 'skarab') client.say(target, "Va suivre Skarab ou j'te tape Kappa (https://www.twitch.tv/skarab42)")
+    if (commandName === config.prefix + 'purple') client.say(target, "Va suivre Purple ou j'te tape Kappa (https://www.twitch.tv/purpleorwel)")
     if (commandName === config.prefix + 'git') client.say(target, 'mon Github => https://github.com/Kae-Tempest')
-    if (commandName === config.prefix + 'hi Skarab') client.say(target, '!say Salut Skarab')
-    if (commandName === config.prefix + 'battle') client.say(target, 'attend je dev ca, laisse moi le temps wesh')
-    if(commandName === config.prefix + 'help') client.say(target, `La liste des commandes est : ${(commandNameList).join(' || ')}`)
-
 });
 client.on('connected', () => {
     let now = new Date().toLocaleString('fr-FR')
