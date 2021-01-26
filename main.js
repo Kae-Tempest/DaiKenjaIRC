8e751cfd244ca2b176c293879fcca68da9457bb4
xLeiry <kyllianlegrand78@gmail.com>
2021-01-26 11:29:07 +0100

    add somethings

--- main.js
+++ main.js
@@ -29,10 +29,13 @@ client.on('message', (target, context, msg, self) => {
     if (self) return;
     const commandName = msg.trim();
     const discordLink = 'https://discord.gg/V9t5k5z'
+    commandNamelist = ['discord','skarab','purple','git','project']
     if (commandName === config.prefix + 'discord') client.say(target, `Lien pour rejoindre mon discord => ${discordLink}`)
     if (commandName === config.prefix + 'skarab') client.say(target, "Va suivre Skarab ou j'te tape Kappa (https://www.twitch.tv/skarab42)")
     if (commandName === config.prefix + 'purple') client.say(target, "Va suivre Purple ou j'te tape Kappa (https://www.twitch.tv/purpleorwel)")
     if (commandName === config.prefix + 'git') client.say(target, 'mon Github => https://github.com/Kae-Tempest')
+    if (commandName === config.prefix + 'project') client.say(target, 'Mon project un un RPG, actuellement disponible sur Discord, au futur disponible sur web, mobile et twitch.')
+    if (commandName === config.prefix + 'help') client.say(target, `${commandNamelist.join(' || ')}`)
 });
 client.on('connected', () => {
     let now = new Date().toLocaleString('fr-FR')
